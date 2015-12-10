4ebdbeff2a218a6e4dc333a7df2f304678b93c3a
Shawn O'Hare <ohare.shawn@gmail.com>
2015-12-10 10:16:15 -0800

    Simplify logger.

--- wemdigo.go
+++ wemdigo.go
@@ -3,19 +3,11 @@ package wemdigo
 import (
 	"log"
 	"os"
+	"strings"
 )
 
 func logger() func(string, ...interface{}) {
-	debug := os.Getenv("DEBUG")
-	modes := strings.Split(debug, ",")
-	var ok bool
-	for _, mode := range modes {
-		if mode == "wemdigo" {
-			ok = true
-			break
-		}
-	}
-	if ok {
+	if strings.Contains(os.Getenv("DEBUG"), "wemdigo") {
 		f := func(format string, v ...interface{}) {
 			wemdigoStr := "\033[0;32m" + "wemdigo" + "\033[0m "
 			log.Printf(wemdigoStr+format, v...)
